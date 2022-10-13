948fe0bcceb553f4109ccfdfbd48aaa61c239069
hbhawsar777 <54030707+hbhawsar777@users.noreply.github.com>
2022-10-13 11:34:03 +0530

    Update add.c

--- add.c
+++ add.c
@@ -15,3 +15,15 @@ int main() {
     printf("%d - %d = %d", number1, number2, diff );
     return 0;
 }
+int remove_duplicate_elements(int arr[], int n)
+{
+
+if (n==0 || n==1)
+return n;
+
+int temp[n];
+
+int j = 0;
+int i;
+for (i=0; i<n-1; i++)
+}
