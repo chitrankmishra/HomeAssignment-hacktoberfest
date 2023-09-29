04ffe9c2e69701466d92e3a10996c33f3caea523
rajneeshkumar1507 <rk1441933@gmail.com>
2023-09-29 18:37:51 +0530

    index.js modified

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function initialize() {
 		setCookie('theme', 'dark', 1);
 	}
 }
+function rajneesh(){
+	console.log("rajneesh");
+}
 
 function validateEmail(email) {
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
