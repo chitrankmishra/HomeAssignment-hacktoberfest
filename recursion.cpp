33bafc5a3010045c1e18cfefc1a5a582da0dff4c
prasannaelectrotech <67099812+prasannaelectrotech@users.noreply.github.com>
2023-10-09 15:52:07 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -6,6 +6,36 @@ int fun1(int x)
 {
 
 	if(x>0){
+
+int y = 0;
+int fun1(int x)
+{
+
+	if(x>0){
+
+int y = 0;
+int fun1(int x)
+{
+
+	if(x>0){
+
+int y = 0;
+int fun1(int x)
+{
+
+	if(x>0){
+
+int y = 0;
+int fun1(int x)
+{
+
+	if(x>0){
+
+int y = 0;
+int fun1(int x)
+{
+
+	if(x>0){
 		fun1(x-1);
 		cout<<x<<"\n";
 		y++;
