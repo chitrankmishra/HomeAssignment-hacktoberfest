2658ebd263246f8c7faec7762c2e6fc442d5d199
akmax102 <115563676+akmax102@users.noreply.github.com>
2023-10-10 10:24:18 +0530

    Update template2.js

--- template2.js
+++ template2.js
@@ -10,6 +10,56 @@ function initialize() {
 }
 
 function validateEmail(email) {
+		theme = getCookie('theme');
+		if (theme == 'light') {
+			toggleTheme();
+		}
+	} else {
+		setCookie('theme', 'dark', 1);
+	}
+}
+
+function validateEmail(email) {
+		theme = getCookie('theme');
+		if (theme == 'light') {
+			toggleTheme();
+		}
+	} else {
+		setCookie('theme', 'dark', 1);
+	}
+}
+
+function validateEmail(email) {
+		theme = getCookie('theme');
+		if (theme == 'light') {
+			toggleTheme();
+		}
+	} else {
+		setCookie('theme', 'dark', 1);
+	}
+}
+
+function validateEmail(email) {
+		theme = getCookie('theme');
+		if (theme == 'light') {
+			toggleTheme();
+		}
+	} else {
+		setCookie('theme', 'dark', 1);
+	}
+}
+
+function validateEmail(email) {
+		theme = getCookie('theme');
+		if (theme == 'light') {
+			toggleTheme();
+		}
+	} else {
+		setCookie('theme', 'dark', 1);
+	}
+}
+
+function validateEmail(email) {
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
 }
