b6880e064c17b145a18490246097e6e954761f35
akmax102 <115563676+akmax102@users.noreply.github.com>
2023-10-10 10:22:34 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -12,6 +12,33 @@ ll pow(ll m, ll n)
     if (n == 0)
         return 1;
     if (n & 1)
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+ll pow(ll m, ll n)
+{
+    if (n == 0)
+        return 1;
+    if (n & 1)
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+ll pow(ll m, ll n)
+{
+    if (n == 0)
+        return 1;
+    if (n & 1)
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+ll pow(ll m, ll n)
+{
+    if (n == 0)
+        return 1;
+    if (n & 1)
         return m * pow(m, n / 2) * pow(m, n / 2);
     else
         return pow(m, n / 2) * pow(m, n / 2);
